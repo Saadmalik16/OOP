2b4c94ed4f90ff8765038499cf4a9d88a1bfc834
Saadmalik16 <57633968+Saadmalik16@users.noreply.github.com>
2019-12-12 16:36:24 +0500

    Add files via upload

---
+++ Ch no 5/5.15.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int j;
+int main ()
+{
+	//for (int i=0; i<10; i++)
+	//{
+	//	for (int j=0; j<i; j++)
+	//	cout << "*" ;
+	//}
+	//cout << endl;
+	//for (int i=9; i>0; i--)
+	//{
+	//	for (j=0;j<i;j++)
+	//	{
+	//		cout << "*";
+	//	}
+	//	cout <<endl;
+	//	for (int k=10; k>j; k--)
+	//	{
+	//		cout << " ";
+	//	}
+		//cout <<endl;
+	//}
+	//	for (int i=9; i>0; i--)
+	//{
+	//	for (j=0;j<i;j++)
+	//	{
+	//		cout << " ";
+	//	}
+	//	for (int k=10; k>j; k--)
+	//	{
+	//		cout << "*";
+	//	}
+	//		cout <<endl;
+		//cout <<endl;
+	//}
+}
